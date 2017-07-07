b94b681da08246c27bf07292776e678e7eb796c0
Burak KOSE <burakks41@gmail.com>
2017-07-07 23:38:33 +0200

    Add basic defination of service actions

---
+++ src/main/scala/io/github/quark/operation/ServiceAction.scala
@@ -0,0 +1,21 @@
+package io.github.quark.operation
+
+import shapeless._
+import shapeless.ops.hlist.IsHCons
+
+sealed trait ServiceAction
+
+object ServiceAction {
+
+  final case class Service[L <: HList](path: String)(ops: L)(
+      implicit lUBConstraint: LUBConstraint[L, OperationAction],
+      isDistinctConstraint: IsDistinctConstraint[L],
+      isHCons: IsHCons[L])
+      extends ServiceAction {
+    def operation[T <: OperationAction](
+        implicit operationSelector: OperationSelector[L, T]): T = {
+      operationSelector(ops)
+    }
+  }
+
+}
