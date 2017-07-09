853e5f58ad30a0a7d9097c5fd3a4e8f501e6c11f
Burak KOSE <burakks41@gmail.com>
2017-07-09 14:36:34 +0200

    Update dsl structure

--- src/main/scala/io/github/quark/dsl/DSL.scala
+++ src/main/scala/io/github/quark/dsl/DSL.scala
@@ -1,8 +1,10 @@
 package io.github.quark.dsl
 
-import io.github.quark.operation.OperationAction
+import io.github.quark.route.{Route, RouteBuilder}
+import io.github.quark.operation.GatewayAction.Gateway
 import io.github.quark.operation.OperationAction._
 import io.github.quark.operation.ServiceAction.Service
+import io.github.quark.operation.{OperationAction, ServiceAction}
 import io.github.quark.stage.PipelineStage.{Input, Output}
 import shapeless.ops.hlist.IsHCons
 import shapeless.{HList, IsDistinctConstraint, LUBConstraint}
@@ -10,7 +12,6 @@ import shapeless.{HList, IsDistinctConstraint, LUBConstraint}
 sealed trait DSL
 
 trait OperationDSL extends DSL {
-
   def incoming(f: OperationF[Input, OperationOutput[Input]]): Incoming =
     Incoming(f)
 
@@ -22,7 +23,7 @@ trait OperationDSL extends DSL {
 
 }
 
-trait ServiceDSL {
+trait ServiceDSL extends DSL {
   def service[L <: HList](path: String)(operations: L)(
       implicit lUBConstraint: LUBConstraint[L, OperationAction],
       isDistinctConstraint: IsDistinctConstraint[L],
@@ -30,4 +31,14 @@ trait ServiceDSL {
     Service(path)(operations)
 }
 
-object DSL extends OperationDSL with ServiceDSL
+trait GatewayDSL extends DSL {
+  def gateway[L1 <: HList, L2 <: HList](services: L1)(
+      implicit builder: RouteBuilder.Aux[L1, L2],
+      isHCons1: IsHCons[L1],
+      isHCons2: IsHCons[L2],
+      lUBConstraint1: LUBConstraint[L1, ServiceAction],
+      lUBConstraint2: LUBConstraint[L2, Route]): Gateway[L2] =
+    Gateway(builder(services))
+}
+
+object DSL extends OperationDSL with ServiceDSL with GatewayDSL
