8648849521ed678ef12ba10ba2a7e112cb762e45
Burak KOSE <burakks41@gmail.com>
2017-07-09 14:29:36 +0200

    Delete experimental implementation of routes

--- src/main/scala/io/github/quark/route/QuarkRouteStatus.scala
+++
@@ -1,8 +0,0 @@
-package io.github.quark.route
-
-sealed trait QuarkRouteStatus
-
-object QuarkRouteStatus {
-  case object Success extends QuarkRouteStatus
-  case object Failure extends QuarkRouteStatus
-}
