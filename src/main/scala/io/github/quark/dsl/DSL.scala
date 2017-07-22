7cd33e12bd905c969f61d7bbb7617cddfec00eb6
Burak KOSE <burakks41@gmail.com>
2017-07-22 23:04:56 +0200

    Integrate dsl with the new implementation of actions

--- src/main/scala/io/github/quark/dsl/DSL.scala
+++ src/main/scala/io/github/quark/dsl/DSL.scala
@@ -1,24 +1,24 @@
 package io.github.quark.dsl
 
-import io.github.quark.route.{Route, RouteBuilder}
 import io.github.quark.action.GatewayAction.Gateway
 import io.github.quark.action.OperationAction._
 import io.github.quark.action.ServiceAction.Service
 import io.github.quark.action.{OperationAction, ServiceAction}
+import io.github.quark.route.{Route, RouteBuilder}
 import io.github.quark.stage.PipelineStage.{Input, Output}
-import shapeless.ops.hlist.IsHCons
+import shapeless.ops.hlist.{IsHCons, ToTraversable}
 import shapeless.{HList, IsDistinctConstraint, LUBConstraint}
 
 sealed trait DSL
 
 trait OperationDSL extends DSL {
-  def incoming(f: OperationF[Input, OperationOutput[Input]]): Incoming =
+  def incoming(f: Input => OperationOutput[Input]): Incoming =
     Incoming(f)
 
-  def outgoing(f: OperationF[Output, OperationOutput[Output]]): Outgoing =
+  def outgoing(f: Output => OperationOutput[Output]): Outgoing =
     Outgoing(f)
 
-  def endpoint(f: OperationF[Input, OperationOutput[Output]]): Endpoint =
+  def endpoint(f: Input => OperationOutput[Output]): Endpoint =
     Endpoint(f)
 
 }
@@ -27,18 +27,18 @@ trait ServiceDSL extends DSL {
   def service[L <: HList](path: String)(operations: L)(
       implicit lUBConstraint: LUBConstraint[L, OperationAction],
       isDistinctConstraint: IsDistinctConstraint[L],
-      isHCons: IsHCons[L]): Service[L] =
-    Service(path)(operations)
+      toTraversableAux: ToTraversable.Aux[L, List, OperationAction],
+      isHCons: IsHCons[L]): Service =
+    Service(path, operations.toList[OperationAction])
 }
 
 trait GatewayDSL extends DSL {
   def gateway[L1 <: HList, L2 <: HList](services: L1)(
       implicit builder: RouteBuilder.Aux[L1, L2],
       isHCons1: IsHCons[L1],
-      isHCons2: IsHCons[L2],
-      lUBConstraint1: LUBConstraint[L1, ServiceAction],
-      lUBConstraint2: LUBConstraint[L2, Route]): Gateway[L2] =
-    Gateway(builder(services))
+      lUBConstraint: LUBConstraint[L1, ServiceAction],
+      toTraversableAux: ToTraversable.Aux[L2, List, Route]): Gateway =
+    Gateway(builder(services).toList[Route])
 }
 
 object DSL extends OperationDSL with ServiceDSL with GatewayDSL
