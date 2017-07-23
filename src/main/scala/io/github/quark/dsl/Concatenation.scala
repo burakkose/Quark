04d7fab0822c380466ff6221e3343888156ada60
Burak KOSE <burakks41@gmail.com>
2017-07-24 00:03:24 +0200

    Add nice concatenation with tilde sign

---
+++ src/main/scala/io/github/quark/dsl/Concatenation.scala
@@ -0,0 +1,20 @@
+package io.github.quark.dsl
+
+import io.github.quark.action.{OperationAction, ServiceAction}
+import shapeless.{::, HList, HNil}
+
+object Concatenation {
+
+  implicit class Concatenation[P, L <: HList](l: L) {
+    def ~[M <: P](other: M): M :: L = other +: l
+  }
+
+  implicit def enhanceOperationWithConcatenation[P <: OperationAction](
+      action: P): Concatenation[OperationAction, P :: HNil] =
+    new Concatenation[OperationAction, P :: HNil](action :: HNil)
+
+  implicit def enhanceServiceWithConcatenation[P <: ServiceAction](
+      action: P): Concatenation[ServiceAction, P :: HNil] =
+    new Concatenation[ServiceAction, P :: HNil](action :: HNil)
+
+}
