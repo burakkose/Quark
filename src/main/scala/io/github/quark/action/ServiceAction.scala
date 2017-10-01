8ebab855acdc2677c3f9fa3663dc47a65a33c9bb
Burak KOSE <burakks41@gmail.com>
2017-10-01 16:46:55 +0200

    Use shapeless Typeable for finding operation in a service

--- src/main/scala/io/github/quark/action/ServiceAction.scala
+++ src/main/scala/io/github/quark/action/ServiceAction.scala
@@ -1,22 +1,23 @@
 package io.github.quark.action
 
-import scala.reflect.ClassTag
+import shapeless.{TypeCase, Typeable}
 
 sealed trait ServiceAction {
   def id: String
 
   def ops: Seq[OperationAction]
 
-  def operation[T <: OperationAction](implicit tag: ClassTag[T]): Option[T]
+  def operation[T <: OperationAction: Typeable]: Option[T]
 }
 
 object ServiceAction {
 
   final case class Service(id: String, ops: Seq[OperationAction])
       extends ServiceAction {
-    def operation[T <: OperationAction](implicit tag: ClassTag[T]): Option[T] = {
+    def operation[T <: OperationAction: Typeable]: Option[T] = {
+      val actionType = TypeCase[T]
       ops.flatMap {
-        case operation: T => Some(operation)
+        case actionType(action) => Some(action)
         case _ => None
       }.headOption
     }
